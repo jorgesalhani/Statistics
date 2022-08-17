0a21277ef02e4276a2a39a02932119f562fe2d3c
jorgesalhani <jorge.salhani@usp.br>
2022-08-17 16:51:41 -0300

    Start expected value plot

--- Topics/T01/script.js
+++ Topics/T01/script.js
@@ -8,15 +8,16 @@ function getFairDiceOccurrences(N) {
   return diceFrequecyDistribution;
 }
 
-function fairDiceHistogram() {
-  let margin = {top: 10, right: 30, bottom: 30, left: 40};
+function expectedValuePlot(data) {
+  let margin = {top: 10, right: 30, bottom: 50, left: 60};
   let width = 460 - margin.left - margin.right;
   let height = 400 - margin.top - margin.bottom;
   
   let svgElement = document.createElement('div');
   svgElement.classList.add('data-viz')
-  let t01Element = document.querySelector('.T01');
+  let t01Element = document.querySelector('.plot-container');
   t01Element.appendChild(svgElement);
+  svgElement.setAttribute('style', `width:${width}px;`);
 
   let svg = d3.select(".data-viz")
     .append("svg")
@@ -26,7 +27,6 @@ function fairDiceHistogram() {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   let nSample = 10;
-  let data = getFairDiceOccurrences(nSample);
   let x = d3.scaleLinear()
     .domain([1, 7])     
     .range([1, width]);
@@ -35,7 +35,22 @@ function fairDiceHistogram() {
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
-  console.log(x.domain());
+  svg.append("text")             
+    .attr("transform",
+          "translate(" + (width/2) + " ," + 
+                          (height + margin.top + 30) + ")")
+    .style("text-anchor", "middle")
+    .text("Face i");
+  
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Frequência (%)"); 
+
+  // console.log(x.domain());
   let histogram = d3.histogram()
       .value(function(d) {
         return d; 
@@ -48,7 +63,7 @@ function fairDiceHistogram() {
   let y = d3.scaleLinear()
     .range([height, 0])
     .domain([0, d3.max(bins, function(d) { 
-      return d.length / nSample; 
+      return (d.length / nSample + 0.1); 
     })]);
     
   svg.append("g")
@@ -69,13 +84,101 @@ function fairDiceHistogram() {
         })
         .attr("height", function(d) { 
           // console.log(d.length, '--');
-          console.log(x(d.x0));
+          // console.log(x(d.x0));
+          return height - y(d.length / nSample); 
+        })
+        .style("fill", "#69b3a2");
+}
+
+function fairDiceHistogram() {
+  let margin = {top: 10, right: 30, bottom: 50, left: 60};
+  let width = 460 - margin.left - margin.right;
+  let height = 400 - margin.top - margin.bottom;
+
+  let plotContainer = document.createElement('div');
+  plotContainer.classList.add('plot-container');
+  plotContainer.setAttribute('style', 'display:flex;flex-wrap:nowrap');
+  
+  let svgElement = document.createElement('div');
+  svgElement.classList.add('hist-viz')
+  let t01Element = document.querySelector('.T01');
+  t01Element.appendChild(plotContainer);
+
+  plotContainer.appendChild(svgElement);
+  svgElement.setAttribute('style', `width:${width}px;margin-right:100px`);
+
+  let svg = d3.select(".hist-viz")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  let nSample = 10;
+  let data = getFairDiceOccurrences(nSample);
+  let x = d3.scaleLinear()
+    .domain([1, 7])     
+    .range([1, width]);
+  
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  svg.append("text")             
+    .attr("transform",
+          "translate(" + (width/2) + " ," + 
+                          (height + margin.top + 30) + ")")
+    .style("text-anchor", "middle")
+    .text("Face i");
+  
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Frequência (%)"); 
+
+  // console.log(x.domain());
+  let histogram = d3.histogram()
+      .value(function(d) {
+        return d; 
+      })
+      .domain(x.domain())
+      .thresholds(x.ticks(6));
+
+  let bins = histogram(data);
+
+  let y = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, d3.max(bins, function(d) { 
+      return (d.length / nSample + 0.1); 
+    })]);
+    
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+  // append the bar rectangles to the svg element
+  bins.pop()
+  svg.selectAll("rect")
+      .data(bins)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { 
+          return "translate(" + x(d.x0) + "," + y(d.length / nSample) + ")"; 
+        })
+        .attr("width", function(d) { 
+          return x(d.x1) - x(d.x0)- 1;
+        })
+        .attr("height", function(d) { 
+          // console.log(d.length, '--');
+          // console.log(x(d.x0));
           return height - y(d.length / nSample); 
         })
         .style("fill", "#69b3a2");
 
-    console.log(data);
-    console.log(bins);
+  expectedValuePlot(data);
 }
 
 let t01Element = document.querySelector('.T01');
